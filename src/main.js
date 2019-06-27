4a30cd87d3b7a552fa83b191276a93fcd78151e2
shan0819 <1576414253@qq.com>
2019-06-27 20:12:15 +0800

    创建项目完成

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from "vue";
+import ElementUI from "element-ui";
 import App from "./App.vue";
 import router from "./router";
 
-Vue.config.productionTip = false;
+Vue.use(ElementUI);
 
 new Vue({
   router,
